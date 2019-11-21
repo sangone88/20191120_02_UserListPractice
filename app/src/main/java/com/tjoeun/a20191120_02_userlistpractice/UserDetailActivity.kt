f9132c919bfca6acee0fb83064578476177b3b1e
sangone88 <llil1@naver.com>
2019-11-21 20:12:44 +0900

    스피너 실제 데이터 반영

--- app/src/main/java/com/tjoeun/a20191120_02_userlistpractice/UserDetailActivity.kt
+++ app/src/main/java/com/tjoeun/a20191120_02_userlistpractice/UserDetailActivity.kt
@@ -4,11 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.tjoeun.a20191120_02_userlistpractice.adapters.CategorySpinnerAdapter
+import com.tjoeun.a20191120_02_userlistpractice.daters.Category
 import com.tjoeun.a20191120_02_userlistpractice.utils.ConnectServer
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_user_detail.*
 import org.json.JSONObject
+import java.util.*
+import kotlin.collections.ArrayList
 
 class UserDetailActivity : BaseActivity() {
+
+    var categoryList = ArrayList<Category>()
+    var categorySpinnerAdapter:CategorySpinnerAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_detail)
@@ -22,6 +31,9 @@ class UserDetailActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        categorySpinnerAdapter = CategorySpinnerAdapter(mContext, categoryList)
+        categorySelectSpinner.adapter = categorySpinnerAdapter
+
         getCategoryListFromServer()
 
     }
@@ -30,6 +42,23 @@ class UserDetailActivity : BaseActivity() {
         ConnectServer.getRequestCategoryList(mContext, object : ConnectServer.JsonResponseHandler {
             override fun onResponse(json: JSONObject) {
                 Log.d("카테고리응답", json.toString())
+
+                var code = json.getInt("code")
+                if (code ==200) {
+                    var data = json.getJSONObject("data")
+                    var userCategories = data.getJSONArray("user_categories")
+
+                    categoryList.clear()
+                    for (i in 0..userCategories.length()-1) {
+                        var uc = userCategories.getJSONObject(i)
+                        var categoryData = Category.getCategoryFromJson(uc)
+                        categoryList.add(categoryData)
+                    }
+
+                    runOnUiThread {
+                        categorySpinnerAdapter?.notifyDataSetChanged()
+                    }
+                }
             }
 
         })
---
+++ app/src/main/java/com/tjoeun/a20191120_02_userlistpractice/adapters/CategorySpinnerAdapter.kt
@@ -0,0 +1,68 @@
+package com.tjoeun.a20191120_02_userlistpractice.adapters
+
+import android.content.Context
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.tjoeun.a20191120_02_userlistpractice.R
+import com.tjoeun.a20191120_02_userlistpractice.daters.Category
+import com.tjoeun.a20191120_02_userlistpractice.daters.User
+
+
+class CategorySpinnerAdapter(context: Context, res:Int, list:ArrayList<Category>)
+    : ArrayAdapter<Category> (context, res, list) {
+
+    var mContext = context
+    var mList = list
+    var inf = LayoutInflater.from(mContext)
+
+    constructor(context: Context, list: ArrayList<Category>) : this(context, R.layout.category_list_spiner_item, list)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.category_list_spiner_item, null)
+        }
+
+        var row = tempRow!!
+
+        var categoryColorImg = row.findViewById<ImageView>(R.id.categoryColorImg)
+        var categoryTitleTxt = row.findViewById<TextView>(R.id.categoryTitleTxt)
+
+        var data = mList.get(position)
+
+        categoryTitleTxt.text = data.title
+
+//        String => Color로 변경.
+        categoryColorImg.setBackgroundColor(Color.parseColor(data.color))
+
+
+        return row
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.category_list_spiner_item, null)
+        }
+
+        var row = tempRow!!
+
+        var categoryColorImg = row.findViewById<ImageView>(R.id.categoryColorImg)
+        var categoryTitleTxt = row.findViewById<TextView>(R.id.categoryTitleTxt)
+
+        var data = mList.get(position)
+
+        categoryTitleTxt.text = data.title
+
+//        String => Color로 변경.
+        categoryColorImg.setBackgroundColor(Color.parseColor(data.color))
+
+
+        return row
+    }
+}
